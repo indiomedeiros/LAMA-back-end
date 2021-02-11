320ddc68f074bce2b520ad2dcdc7c68f7c2b9f09
Diana Monteiro <dianar.monteiro@gmail.com>
2021-02-11 16:02:44 -0300

    Adicionando entidades

---
+++ lama-template/src/bussines/entities/Band.ts
@@ -0,0 +1,30 @@
+export class Shows {
+    constructor(
+        public readonly id: string,
+        public readonly week_day: string,
+        public readonly start_time: number,
+        public readonly end_time: number,
+        public readonly band_id: string
+    ) { }
+}
+
+export class Band {
+    constructor(
+        public readonly id: string,
+        public readonly name: string,
+        public readonly music_genre: string,
+        public readonly responsible: string
+    ) { }
+}
+
+export interface BandInputDTO {
+    name: string;
+    music_genre: string;
+    responsible: string;
+}
+
+export interface DetailsInputDTO {
+    id: string;
+    name: string;
+}
+
